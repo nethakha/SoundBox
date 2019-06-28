e12c3c3101804d8b97a058d333086cc3f4c1c6d2
nethakha <tokuteikoway@gmail.com>
2019-06-28 13:02:29 +0900

    プロジェクト ファイルを追加します。

---
+++ SoundBox/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SoundBox {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Sounds = new System.Windows.Forms.ListBox();
+            this.messageLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(145, 196);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(8, 8);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // Sounds
+            // 
+            this.Sounds.FormattingEnabled = true;
+            this.Sounds.ItemHeight = 12;
+            this.Sounds.Location = new System.Drawing.Point(13, 37);
+            this.Sounds.Name = "Sounds";
+            this.Sounds.Size = new System.Drawing.Size(185, 400);
+            this.Sounds.TabIndex = 1;
+            this.Sounds.SelectedIndexChanged += new System.EventHandler(this.SetSoundsBox_SelectedIndexChanged);
+            this.Sounds.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Sounds_KeyDown);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Location = new System.Drawing.Point(13, 13);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(35, 12);
+            this.messageLabel.TabIndex = 2;
+            this.messageLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.Sounds);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox Sounds;
+        private System.Windows.Forms.Label messageLabel;
+    }
+}
+
---
+++ SoundBox/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+using System.IO;
+using AxWMPLib;
+
+namespace SoundBox {
+    public partial class Form1 : Form {
+
+        private const string location = @"..\..\sounds\";
+        private string fileName;
+
+        public Form1() {
+            InitializeComponent();
+
+            KeyPreview = true; // キー入力をFormへ渡す
+            messageLabel.Text = "音源を選択してください。";
+
+            // ListBox Sounds へデータ追加
+            string[] files = Directory.GetFiles(location, "*", SearchOption.AllDirectories);
+            string[] filesName = new string[files.Count()];
+
+            foreach(var f in files.Select((item, index) => new { item, index })) {
+                filesName[f.index] = Path.GetFileName(f.item);
+            }
+
+            Sounds.Items.AddRange(filesName);
+            createInstance();
+        }
+
+        WindowsMediaPlayer[] WMP;
+        private void createInstance() {
+            int elm = 30;
+            WMP = new WindowsMediaPlayer[elm];
+            for (int i = 0; i < elm; i++) {
+                WMP[i] = new WindowsMediaPlayer();
+            }
+        }
+
+        private void playSound() {
+            int i = 0;
+            int stoppedNumber = -1;
+            foreach(WindowsMediaPlayer w in WMP) {
+                if(w.playState != WMPPlayState.wmppsPlaying) { //stopped
+                    stoppedNumber = i;
+                    break;
+                }
+                i++;
+            }
+
+            if(stoppedNumber != -1) {
+                WMP[stoppedNumber].URL = Path.GetFullPath(location) + fileName;
+                WMP[stoppedNumber].controls.play();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            messageLabel.Text = e.KeyCode + "が押されました。";
+
+            if(fileName != null) {
+                playSound();
+            }
+        }
+
+        private void SetSoundsBox_SelectedIndexChanged(object sender, EventArgs e) {
+            ListBox ls = sender as ListBox;
+            fileName = Sounds.Text;
+            messageLabel.Text = location + fileName + "が選択されました。";
+        }
+
+        private void Sounds_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.Down) return;
+
+            e.SuppressKeyPress = true;
+        }
+    }
+}
